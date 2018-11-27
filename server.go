cafd5a19f6bb2dc247583a8438bd6fa90ff63b4d
mikefaraponov <mikefaraponov@gmail.com>
2018-11-27 20:42:16 +0200

    Added more sophisticated counter

--- server.go
+++ server.go
@@ -17,11 +17,12 @@ type (
 )
 
 func (s *server) Index(ctx echo.Context) error {
-	result, err := s.Incr(ctx.Request().UserAgent()).Result()
+	userAgent := ctx.Request().UserAgent()
+	result, err := s.Incr(userAgent).Result()
 	if err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
-	return ctx.String(http.StatusOK, strconv.FormatInt(result, 10))
+	return ctx.String(http.StatusOK, userAgent + ": " + strconv.FormatInt(result, 10))
 }
 
 func NewServer(client *redis.Ring) Server {
